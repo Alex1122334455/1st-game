e86a09245b91613e775a08baba5ece43610aad0d
Alexandra <amerrem.de@mysummitps.org>
2016-10-20 18:15:42 +0000

    update

--- mygame/myGame.js
+++ mygame/myGame.js
@@ -8,7 +8,7 @@ var cursors;
 var stars;
 var score = 0;
 var scoreText;
-var hitPlatform;
+var platform1;
 
 function preload() {
     
@@ -17,27 +17,44 @@ function preload() {
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.image('space', 'assets/large.png');
+    game.load.image('ground1', 'assets/platform1.png');
 }
 game.physics.startSystem(Phaser.Physics.ARCADE);
 function create() {
     game.add.sprite(0,0, 'space');
     platforms = game.add.group();
     platforms.enableBody = true;
-    var ground = platforms.create(0, game.world.height - 90, 'ground');
+    platform1 = game.add.group();
+    platform1.enableBody = true;
+    var ground = platforms.create(65.5, game.world.height - 64, 'ground');
     ground.scale.setTo(0.5,1);
     ground.body.immovable = true;
-    var ground = platforms.create(400, game.world.height - 90, 'ground');
+    var ground = platforms.create(400, game.world.height - 64, 'ground');
     ground.scale.setTo(0.5,1);
+    //false
+    ground.body.immovable = false;
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    ground.scale.setTo(0.05, 1);
     ground.body.immovable = true;
+    var ground = platforms.create (40, game.world.height - 500, 'ground');
+   ground.scale.setTo(0.5, 1);
+    ground.body.immovable = true;
+    var ground = platforms.create(425, game.world.height - 450, 'ground');
+    ground.scale.setTo(0.87,  1);
+    //false
+    ground.body.immovable = false;
+    var ground = platforms.create(100, game.world.height - 200, 'ground');
+    ground.scale.setTo(0.5, 1);
+    //false
+    ground.body.immovable= false
     var ledge = platforms.create(280, 370, 'ground');
-    ledge.body.immovable = false;
-    var ledge = platforms.create (100, 400, 'ground');
+    //false
     ledge.body.immovable = false;
     var ledge = platforms.create (280, 400, 'ground');
+    //false
     ledge.body.immovable = false;
     var ledge = platforms.create(130, 250, 'ground');
-    ledge.body.immovable = false;
-    var ledge = platforms.create(300, 150, 'ground');
+    //false
     ledge.body.immovable = false;
     var ledge = platforms.create(500, 50, 'ground');
     ledge.body.immovable = true
@@ -45,7 +62,7 @@ function create() {
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0.5;
     player.body.gravity.y = 150;
-    player.body.collideWorldBounds = false;
+    player.body.collideWorldBounds = true;
     player.animations.add('left', [0, 1, 2, 3, ], 10, true);
     player.animations.add('right', [5, 6, 7, 8, ], 10, true);
     cursors = game.input.keyboard.createCursorKeys();
@@ -53,12 +70,12 @@ function create() {
     stars.enableBody = true;
     for (var i = 0; i < 100; i++) 
     {
-        var star = stars.create(i * 8, 0, 'star');
-        star.body.gravity.y = 10;
-        star.body.bounce.y = 0.6 + Math.random() * 0.2;
+    var star = stars.create(i * 8, 0, 'star');
+    star.body.gravity.y = 10;
+    star.body.bounce.y = 0.6 + Math.random() * 0.2;
     }
-    scoreText = game.add.text(650, 550, 'Score: 0', {fontSize: '32px', fill: '#eeceff' });
-}
+    scoreText = game.add.text(550, 550, 'Score: 0', {fontSize: '32px', fill: '#9d88d8' });
+    }
 
 function update() {
     var hitPlatform = game.physics.arcade.collide(player, platforms);
@@ -92,3 +109,12 @@ function collectStar (player, star) {
  score += 1;
  scoreText.text = "score: " + score; + "Score: ";
 }
+/*if(function collectStar (player, star) {
+ star.kill();
+ score += 1;
+ scoreText.text = "score: " + score; + "Score: ";
+ } && score = 100)
+ {
+score.shift();
+score.push("You Win!")
+ }*/
